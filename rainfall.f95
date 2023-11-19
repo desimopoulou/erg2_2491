ec7e1805243b02f332fe12074503a7788505a556
Clab8 <desimopoulou@uth.gr>
2023-11-19 07:05:09 +0200

    c4 - Read and display all items (up to 50) from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,16 +1,29 @@
 program datatxt
 implicit none
 
-integer::i
-character(100),dimension(5)::town
-integer,dimension(5)::Average_Yearly_Rainfall
-
-open(10,file="data.txt")
-read(10,*)
-	do i=1,5
+integer::i,iostatus,Nmax,r
+character(100),dimension(50)::town
+integer,dimension(50)::Average_Yearly_Rainfall
+Nmax=50
+r=0 !rows
+i=1 !counter
+open(10,file="data.txt",iostat=iostatus)
+do   	
+  read(10,*,iostat=iostatus)
+  if (iostatus/=0)exit !error opening the file
+  r=r+1 !counts the number of lines exist
+  !print*, r !result:20 rows with the titles
+end do
+  rewind(10) !goes the file from the beginning
+if (r<=Nmax)then
+  read(10,*)
+  do i=1,r-1 !19 towns
     read(10,*)town(i),Average_Yearly_Rainfall(i)
     write(*,*)town(i),Average_Yearly_Rainfall(i)
-    end do
+  end do
+else
+  print*, "Error"
+end if
 
 close(10)
 end program
